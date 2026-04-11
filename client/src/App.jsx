51575a915d18a4ef71811f7bc3cfafa7322f5189
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-11 17:29:14 +0500

    someissue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+// import { decrement, increment } from './counterSlice'
+
+
+
 
 const App = () => {
-  return (<>
-   
-  </>  )
+  const count = useSelector((state) => state.counter.value)
+  // const dispatch = useDispatch()
+
+
+  return (
+    <>
+       <h1>hu {count}</h1>
+    </>
+  )
 }
 
 export default App
