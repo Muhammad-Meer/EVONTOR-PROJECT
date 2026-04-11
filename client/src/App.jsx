3d97905b6ddf6088f04bee48843da4c3dc80c69c
Muhabbat ali <sadbadbadbbadluck@gmail.com>
2026-04-11 19:30:43 +0500

    newcode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { decrement, increment } from './counter/counterSlice'
+import Navbar from './Navbar'
 
 
 
@@ -15,6 +16,8 @@ const App = () => {
        <h1>hu {count}</h1>
        <button onClick={ () => dispatch(increment())}>+</button>
        <button onClick={ () => dispatch(decrement())}>+</button>
+           <Navbar/>
+
     </>
   )
 }
